4cc0cdd4dc1f1b3b1cf03b711753904a17d4d052
Matthew Wilding <mbwilding@gmail.com>
2023-09-18 17:43:14 +0800

    Match

--- rust-aws-azure-login/src/main.rs
+++ rust-aws-azure-login/src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::anyhow;
+use anyhow::Result;
 use headless_chrome::protocol::cdp::Target::CreateTarget;
 use headless_chrome::{Browser, LaunchOptions};
 use maplit::hashmap;
@@ -6,7 +8,7 @@ use tracing::debug;
 pub mod helpers;
 mod login;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     tracing_subscriber::fmt()
         //.json()
         .with_max_level(tracing::Level::DEBUG)
@@ -14,12 +16,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_line_number(true)
         .init();
 
-    let config = aws::aws_config::AwsConfig::profile_default();
+    let config = aws::aws_config::AwsConfig::profile_default()?;
 
-    let url = login::create_login_url(
-        "https://signin.aws.amazon.com/saml",
-        "308d2158-f6df-4747-8791-e970657274d5",
-    )?;
+    let app_id_uri = match config.azure_app_id_uri {
+        Some(x) => x,
+        None => {
+            return Err(anyhow!("azure_app_id_uri not set"));
+        }
+    };
+
+    let azure_tenant_id = match config.azure_tenant_id {
+        Some(x) => x,
+        None => {
+            return Err(anyhow!("azure_tenant_id not set"));
+        }
+    };
+
+    let azure_default_username = match config.azure_default_username {
+        Some(x) => x,
+        None => {
+            return Err(anyhow!("azure_default_username not set"));
+        }
+    };
+
+    let url = login::create_login_url(&app_id_uri, &azure_tenant_id)?;
     debug!("Opening: {}", url);
 
     let width = 425;
@@ -61,7 +81,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     debug!("Clicking username field");
     field.click()?;
     debug!("Entering username");
-    tab.send_character(&config?.azure_default_username.unwrap())?;
+    tab.send_character(&azure_default_username)?;
     debug!("Finding next button");
     let button = tab.wait_for_element("input#idSIButton9")?;
     debug!("Clicking next button");
